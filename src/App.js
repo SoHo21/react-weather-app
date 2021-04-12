7d1e0f89c4548e2cee7eea64cb414280b1672652
SoHo21 <79358586+SoHo21@users.noreply.github.com>
2021-04-12 09:24:31 +0200

    Added GitHub repo-url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Sofia and is {""}
+      <a href="https://github.com/SoHo21/react-weather-app" target="_blank">open-sourced on GitHub</a>
+      </footer>
     </div>
+    
   );
 }
