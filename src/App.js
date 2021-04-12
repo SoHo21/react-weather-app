d24dc5008b996517e55e2dea1a775327442890f0
SoHo21 <79358586+SoHo21@users.noreply.github.com>
2021-04-12 09:50:03 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by Sofia and is {""}
-      <a href="https://github.com/SoHo21/react-weather-app" target="_blank">open-sourced on GitHub</a>
+      <a href="https://github.com/SoHo21/react-weather-app" rel="noreferrer" target="_blank">open-sourced on GitHub</a>
       </footer>
     </div>
     
