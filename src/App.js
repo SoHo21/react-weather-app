4fbb26f6a88432a14f6a8baf0a860888ffa10123
SoHo21 <79358586+SoHo21@users.noreply.github.com>
2021-04-12 11:51:04 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <div>
-      <h1>Weather App</h1>
+      <div className="container">
+      <Weather />
       <footer>
         This project was coded by Sofia and is {""}
       <a href="https://github.com/SoHo21/react-weather-app" rel="noreferrer" target="_blank">open-sourced on GitHub</a>
