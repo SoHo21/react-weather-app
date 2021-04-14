45dcd4b6459c693628b2d35712062bf380b50374
SoHo21 <79358586+SoHo21@users.noreply.github.com>
2021-04-14 09:09:56 +0200

    Changed static iconUrl to a dynamic Url

--- src/Weather.js
+++ src/Weather.js
@@ -12,7 +12,7 @@ export default function Weather(props) {
             city: response.data.name,
             date: new Date(response.data.dt*1000),
             description: response.data.weather[0].description,
-            iconUrl: "https://www.freeiconspng.com/uploads/sun-icon-16.jpg",
+            iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
             temperature: response.data.main.temp,
             humidity: response.data.main.humidity,
             wind: response.data.wind.speed,
