22dd4b53cb3c6b95a7a2b2982d748b676699684c
SoHo21 <79358586+SoHo21@users.noreply.github.com>
2021-04-15 10:37:14 +0200

    Styled the forecast layout for one day

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
     icon={codeMapping[props.code]}
     color="#d8ac9c"
-    size={52}
+    size={props.size}
     animate={true}
   />
     );
