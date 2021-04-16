f08785cf0093eae90b7104c0fb060665cf0fa4bd
SoHo21 <79358586+SoHo21@users.noreply.github.com>
2021-04-16 09:48:26 +0200

    Added forecast for five days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -13,18 +13,23 @@ export default function WeatherForecast(props) {
     }
 
     if (loaded) {
-        console.log(forecast);
         return (
         <div className="WeatherForecast">
             <div className="row">
-                <div className="col">
-                    <WeatherForecastDay data={forecast[0]} />
-                </div>
+                {forecast.map(function (dailyForecast, index) {
+                    if (index < 5 ) {
+                        return (
+                        <div className="col" key={index}>
+                        <WeatherForecastDay data={dailyForecast} />
+                        </div>
+                    );
+                    }
+                })}
             </div>
         </div>
     );
     } else {
-        let apiKey = "34a66ef508b0cc45fe99cd407595565c";
+    let apiKey = "34a66ef508b0cc45fe99cd407595565c";
     let latitude = props.coordinates.lat;
     let longitude = props.coordinates.lon;
     let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
